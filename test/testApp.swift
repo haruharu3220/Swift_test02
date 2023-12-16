2aec32c58e57fc35bea6f8aa39ef37870b09c37f
Hayato <hayato.314.nico@gmail.com>
2023-12-16 21:51:35 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Yamamoto Hayato on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
